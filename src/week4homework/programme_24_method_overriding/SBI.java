ec8e380a2f15da949581619896fd90af14377923
Ram1998-dn <drampatel19@gmail.com>
2024-11-08 23:24:29 +0000

    week4 homework

---
+++ src/week4homework/programme_24_method_overriding/SBI.java
@@ -0,0 +1,13 @@
+package week4homework.programme_24_method_overriding;
+/**
+ * Test class to create objects and call the methods
+ * Output:
+ * SBI Rate of Interest: 8
+ * ICICI Rate of Interest: 7
+ * AXIS Rate of Interest: 9
+ */
+public class SBI extends Bank{
+    public int getRateOfInterest() {
+        return 8;
+    }
+}
